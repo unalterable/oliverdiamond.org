1a3dce725ef48b1fa49f43f62e0a9073913d8a4f
Oliver Diamond <diamond.oliver@gmail.com>
2016-05-25 18:52:43 +0100

    PHP include test

---
+++ index2.php
@@ -0,0 +1,22 @@
+
+<?php include './theme/header.php';?>
+			<div id="frame-header"><div id="frame-title">Oliver Diamond's Website</div></div>
+			<div class="main-link-container">
+				<a class="main-link" href="about-me/">
+					<img class="main-link-thumbnail" src="images/cube.png">
+					About Me
+				</a>
+			</div>
+			<div class="main-link-container">
+				<a class="main-link" href="makers-academy-blog/">
+					<img class="main-link-thumbnail" src="images/cube.png">
+					Makers Academy Blog
+				</a>
+			</div>
+			<div class="main-link-container">
+				<a class="main-link" href="other-thoughts/">
+					<img class="main-link-thumbnail" src="images/cube.png">
+					Other Thoughts
+				</a>
+			</div>
+<?php include './theme/footer.php';?>
---
+++ theme/header.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="../master-style.css">
+	</head>
+	<body>
+		<div id="header">
+			<div class="top-link linkedin"></div>			
+			<div class="top-link instagram"></div>
+			<div class="top-link twitter"></div>
+		</div>
+		<div id="frame">
